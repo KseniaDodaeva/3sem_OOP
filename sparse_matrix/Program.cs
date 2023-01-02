eec052573b587dcc3575fdd86071e2d247ff7c16
KseniaDodaeva <66111848+KseniaDodaeva@users.noreply.github.com>
2023-01-02 20:43:44 +0300

    Add files via upload

---
+++ sparse_matrix/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using practika_sparse_matrices;
+
+Sparce_matrix<int> matr = new Sparce_matrix<int>(4, 5);
+
+matr[2, 2] = 4;
+matr[0, 2] = 5;
+matr[0, 3] = 6;
+matr[3, 4] = 7;
+matr[1, 2] = 8;
+matr[0, 0] = 9;
+
+matr.show();
+Console.WriteLine();
+matr.sm(0, 4).show();
